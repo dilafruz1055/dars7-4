510843d6d0a84b1411783220dbb7c41771c872e2
Dilafruz Shukurova <dilafruzshukurova36@gmail.com>
2024-04-29 11:01:36 +0500

    dan

---
+++ index.js
@@ -0,0 +1,14 @@
+let dilafruz = ["Dilafruz", 15, true, "Chorsu/gum"];
+
+console.log(dilafruz);
+console.log(dilafruz.length);
+
+let rost = confirm("Manzilngiz tog'rimi?");
+
+if (rost) {
+
+} else {
+    dilafruz.pop();
+}
+
+console.log(dilafruz);
